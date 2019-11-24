5bb88588210155fbf3f5c6a93d1f1b04e8b646cb
justinmoh <me@justinmoh.my>
2019-11-24 18:17:04 +0800

    added paramerter for back-hidden-field

--- src/helpers.php
+++ src/helpers.php
@@ -5,8 +5,10 @@ use JustinMoh\BackpackHelper\Facades\Fields\HiddenField;
 /**
  * Add a hidden field named `http_referrer` that used to redirect
  * after the form persisted.
+ *
+ * @param  string|null  $url
  */
-function add_back_url_hidden_field(): void
+function add_back_url_hidden_field($url = null): void
 {
-    HiddenField::name('http_referrer')->default(request('back_url', url()->previous()));
+    HiddenField::name('http_referrer')->default($url ?: request('back_url', url()->previous()));
 }
